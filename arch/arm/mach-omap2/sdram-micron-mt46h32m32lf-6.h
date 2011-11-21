18880609cf5f436e5f03d60c829bbc7185602fa4
Jack <jacktseng@jorjin.com.tw>
2011-11-21 18:57:06 +0800

    Cleanup "board-panther.c".
    Fixed "dpll3_m2_clk rate change failed: -22" issue by adding the appropriate setting for Micro's sdram @ 200MHz.

--- arch/arm/mach-omap2/sdram-micron-mt46h32m32lf-6.h
+++ arch/arm/mach-omap2/sdram-micron-mt46h32m32lf-6.h
@@ -14,6 +14,30 @@
 #ifndef ARCH_ARM_MACH_OMAP2_SDRAM_MICRON_MT46H32M32LF
 #define ARCH_ARM_MACH_OMAP2_SDRAM_MICRON_MT46H32M32LF
 
+// Jack_20111118: Copy micro's memory settings @ 200MHz from <ROWBOAT>/x-loader/include/asm/arch-omap3/mem.h
+/* optimized timings good for current shipping parts */
+#define SDP_3430_SDRC_RFR_CTRL_100MHz   0x0002da01
+#define SDP_3430_SDRC_RFR_CTRL_133MHz   0x0003de01 /* 7.8us/7.5ns - 50=0x3de */
+#define SDP_3430_SDRC_RFR_CTRL_165MHz   0x0004e201 /* 7.8us/6ns - 50=0x4e2 */
+#define SDP_3430_SDRC_RFR_CTRL_200MHz   0x0005e601 /* 7.8us/5ns - 50=0x5e6 */
+#define MICRON_TDAL_200   6
+#define MICRON_TDPL_200   3
+#define MICRON_TRRD_200   2
+#define MICRON_TRCD_200   3
+#define MICRON_TRP_200    3
+#define MICRON_TRAS_200   8
+#define MICRON_TRC_200   11
+#define MICRON_TRFC_200  15
+#define MICRON_V_ACTIMA_200 ((MICRON_TRFC_200 << 27) | (MICRON_TRC_200 << 22) | (MICRON_TRAS_200 << 18) \
+		| (MICRON_TRP_200 << 15) | (MICRON_TRCD_200 << 12) |(MICRON_TRRD_200 << 9) | \
+		(MICRON_TDPL_200 << 6) | (MICRON_TDAL_200))
+#define MICRON_TWTR_200   2
+#define MICRON_TCKE_200   1
+#define MICRON_TXP_200    2
+#define MICRON_XSR_200   23
+#define MICRON_V_ACTIMB_200 ((MICRON_TCKE_200 << 12) | (MICRON_XSR_200 << 0)) | \
+				(MICRON_TXP_200 << 8) | (MICRON_TWTR_200 << 16)
+
 #include <plat/sdrc.h>
 
 /* Micron MT46H32M32LF-6 */
@@ -47,7 +71,15 @@ static struct omap_sdrc_params mt46h32m32lf6_sdrc_params[] = {
 		.rfr_ctrl    = 0x00025501,
 		.mr	     = 0x00000032,
 	},
+	/* Added 200MHz's setting. Need to fully test */
 	[4] = {
+		.rate	     = 200000000,
+		.actim_ctrla = MICRON_V_ACTIMA_200,
+		.actim_ctrlb = MICRON_V_ACTIMB_200,
+		.rfr_ctrl    = SDP_3430_SDRC_RFR_CTRL_200MHz,
+		.mr	     = 0x00000032,
+	},
+	[5] = {
 		.rate	     = 0
 	},
 };
